8cf92a4bfd4ac6b6921fca4d8c9eb48d30dfa494
Mateusz Asimowicz <mateuszasimowicz@tlen.pl>
2023-04-18 11:20:54 +0200

    AA-1: Create basic application

---
+++ curb-your-expenses/src/test/java/com/mex/curbyourexpenses/CurbYourExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mex.curbyourexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurbYourExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
