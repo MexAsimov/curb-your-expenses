56b8ae6116f558bcdccfd0a700f6980b4351750a
Mateusz Asimowicz <mateuszasimowicz@tlen.pl>
2023-04-18 11:20:54 +0200

    AA-1-Create-basic-application

---
+++ curb-your-expenses/src/main/java/com/mex/curbyourexpenses/CurbYourExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.mex.curbyourexpenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurbYourExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurbYourExpensesApplication.class, args);
+	}
+
+}
